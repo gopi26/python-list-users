fbee7c9c38c32a35e54ea2f56a557ea39a5e1ea4
gopi26 <83392450+gopi26@users.noreply.github.com>
2021-05-10 10:19:50 +0530

    Comment used // instead #

--- users.py
+++ users.py
@@ -1,13 +1,13 @@
 import requests
 
-// Mock API list of users under / path
+# Mock API list of users under / path
 USERS_URL = 'https://myapp.free.beeceptor.com/'
 
-// Mock API list of users under / path printed in main page
+# Mock API list of users under / path printed in main page
 x = requests.get('https://myapp.free.beeceptor.com/')
 print(x.text)
     
-// Unit test case to verify the page return 200 success code
+# Unit test case to verify the page return 200 success code
 def get_users():
     """Get list of users"""
     response = requests.get(USERS_URL)
